451d2dc88c4bf1833e0b40058ef40ac30fde1efe
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-07 00:19:13 -0500

    arreglando errores entre los css

--- explora-peru/src/app/app.ts
+++ explora-peru/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
 })
-export class AppComponent {}
+export class App {}

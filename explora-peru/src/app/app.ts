90593d0bcee190d9a80989c80185654f472eb7b0
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-07 00:10:57 -0500

    creacion de la carpeta assets para las imagenes

--- explora-peru/src/app/app.ts
+++ explora-peru/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
-export class App {
-  protected title = 'explora-peru';
-}
+export class AppComponent {}

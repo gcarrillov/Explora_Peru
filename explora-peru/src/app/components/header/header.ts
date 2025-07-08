e38b922c4e5582abba3ad3b2a5fe6e587a6bef94
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-08 09:17:15 -0500

    se cambio los archivos del header, porque ahora se tienen subcarpetas y adentro archivos para cada boton

--- explora-peru/src/app/components/header/header.ts
+++ explora-peru/src/app/components/header/header.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BotonBusquedaComponent } from '../boton-busqueda/boton-busqueda';
+import { BotonAleatorioComponent } from '../boton-aleatorio/boton-aleatorio';
+import { BotonAboutComponent } from '../boton-about/boton-about';
+import { BotonLoginComponent } from '../boton-login/boton-login';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    BotonBusquedaComponent,
+    BotonAleatorioComponent,
+    BotonAboutComponent,
+    BotonLoginComponent
+  ],
   templateUrl: './header.html',
-  styleUrls: ['./header.css'],
+  styleUrls: ['./header.css']
 })
 export class HeaderComponent {}

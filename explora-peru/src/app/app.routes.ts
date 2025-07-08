45518cac31e70c6f33298ac91858e3d0cd086db4
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-08 18:18:22 -0500

    agregando estructura para las regiones al proyecto, se implmento carpetas para subdividir

--- explora-peru/src/app/app.routes.ts
+++ explora-peru/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ArequipaComponent } from './pages/arequipa/arequipa';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./app').then(m => m.App),
+  },
+  {
+    path: 'region/arequipa',
+    component: ArequipaComponent
+  }
+];
---
+++ explora-peru/src/app/pages/arequipa/arequipa.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arequipa',
+  standalone: true,
+  template: `
+    <div style="padding: 2rem;">
+      <h1>Bienvenido a Arequipa</h1>
+      <p>Explora la región Arequipa, famosa por su cultura, historia y gastronomía.</p>
+    </div>
+  `
+})
+export class ArequipaComponent {}

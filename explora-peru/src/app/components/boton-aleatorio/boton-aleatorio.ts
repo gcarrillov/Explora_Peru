eb331d4e1b6d2d958818195c4bc67ae70b9a9eeb
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-08 11:51:04 -0500

    modificando archivos .ts de todos los botones para que los estilos de cada uno funcione debidamente, se modifico boton-aleatorio.ts

--- explora-peru/src/app/components/boton-aleatorio/boton-aleatorio.ts
+++ explora-peru/src/app/components/boton-aleatorio/boton-aleatorio.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-boton-aleatorio',
   standalone: true,
   templateUrl: './boton-aleatorio.html',
-  styleUrls: ['./boton-aleatorio.css']
+  styleUrls: ['./boton-aleatorio.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BotonAleatorioComponent {}

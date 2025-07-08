9ed4c0f357f7d689e5f32deb6fdcfa20f4b2cd05
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-08 11:45:58 -0500

    modificando archivos .ts de todos los botones para que los estilos de cada uno funcione debidamente, se modificaron : boton-busqueda.ts y boton-about.ts

--- explora-peru/src/app/components/boton-about/boton-about.ts
+++ explora-peru/src/app/components/boton-about/boton-about.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-boton-about',
   standalone: true,
   templateUrl: './boton-about.html',
-  styleUrls: ['./boton-about.css']
+  styleUrls: ['./boton-about.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BotonAboutComponent {}
--- explora-peru/src/app/components/boton-busqueda/boton-busqueda.ts
+++ explora-peru/src/app/components/boton-busqueda/boton-busqueda.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-boton-busqueda',
   standalone: true,
   templateUrl: './boton-busqueda.html',
-  styleUrls: ['./boton-busqueda.css']
+  styleUrls: ['./boton-busqueda.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BotonBusquedaComponent {}

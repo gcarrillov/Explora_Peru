7efcd1d1ddd84e5cb15c63e0dd2d1223c85bf812
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-08 09:12:36 -0500

    se agregaron los archivos ts para los botones login y busqueda

---
+++ explora-peru/src/app/components/boton-login/boton-login.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-login',
+  standalone: true,
+  templateUrl: './boton-login.html',
+  styleUrls: ['./boton-login.css']
+})
+export class BotonLoginComponent {}

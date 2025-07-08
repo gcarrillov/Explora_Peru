5dd0113eca256440e1570c754eacec01ef3ef9d5
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-08 09:11:25 -0500

    se agregaron carpetas especificas para cada boton y con estas los archivos ts para los botones about y aleatorio

---
+++ explora-peru/src/app/components/boton-about/boton-about.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-about',
+  standalone: true,
+  templateUrl: './boton-about.html',
+  styleUrls: ['./boton-about.css']
+})
+export class BotonAboutComponent {}
---
+++ explora-peru/src/app/components/boton-aleatorio/boton-aleatorio.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-aleatorio',
+  standalone: true,
+  templateUrl: './boton-aleatorio.html',
+  styleUrls: ['./boton-aleatorio.css']
+})
+export class BotonAleatorioComponent {}

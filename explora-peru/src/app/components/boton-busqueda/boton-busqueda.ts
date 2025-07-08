dd0940d8b04d286936b3a7b0a3228ee2c0e05cd6
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-08 09:09:37 -0500

    reestructurando el proyecto para mayor organizacion del header

---
+++ explora-peru/src/app/components/boton-busqueda/boton-busqueda.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-busqueda',
+  standalone: true,
+  templateUrl: './boton-busqueda.html',
+  styleUrls: ['./boton-busqueda.css']
+})
+export class BotonBusquedaComponent {}

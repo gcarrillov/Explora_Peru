fb22f0019439c62e7aeac7a8a9db3aa3fd731f34
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-09 08:00:53 -0500

    borrando lineas innecesarias en css

--- explora-peru/src/app/app.ts
+++ explora-peru/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from './components/header/header';  // Importa el componente del header
+import { HeaderComponent } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HeaderComponent],  // Asegúrate de agregarlo aquí
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })

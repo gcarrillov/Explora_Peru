c7ba5619efdb7ac2ac967d3a29cbeb3cc43021fe
sebastianulloasalas <sulloa@unsa.edu.pe>
2025-07-14 01:34:39 -0500

    Igualmente, implementación del modelo Empresa con los campos: Nombre, ruc, telefono y correo

--- backend/regiones/models.py
+++ backend/regiones/models.py
@@ -7,3 +7,14 @@ class Region(models.Model):
 
     def __str__(self):
         return self.nombre
+    
+class Empresa(models.Model):
+    nombre = models.CharField(max_length=100)
+    ruc = models.CharField(max_length=11, unique=True)
+    telefono = models.CharField(max_length=15)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+

e91b610ac4e91885831a0d08305bba36923ecdea
sebastianulloasalas <sulloa@unsa.edu.pe>
2025-07-14 01:33:03 -0500

    Implementación del modelo Región en regiones/models.py con 3 campos, nombres, región y recursos_turísticos

--- backend/regiones/models.py
+++ backend/regiones/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Region(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    recursos_turisticos = models.TextField()
+
+    def __str__(self):
+        return self.nombre

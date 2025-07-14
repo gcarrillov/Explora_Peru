63b74fcc00ff7133eb4e0e596ce585ca2f59417b
sebastianulloasalas <sulloa@unsa.edu.pe>
2025-07-14 01:36:30 -0500

    Implementacion del modelo Ruta con los campos nombre, origen, destino, distancia_km y las relaciones origen y destino siendo una foreignkey de region

--- backend/regiones/models.py
+++ backend/regiones/models.py
@@ -17,4 +17,12 @@ class Empresa(models.Model):
     def __str__(self):
         return self.nombre
 
+class Ruta(models.Model):
+    nombre = models.CharField(max_length=100)
+    origen = models.ForeignKey(Region, related_name='rutas_origen', on_delete=models.CASCADE)
+    destino = models.ForeignKey(Region, related_name='rutas_destino', on_delete=models.CASCADE)
+    distancia_km = models.DecimalField(max_digits=6, decimal_places=2)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.nombre} ({self.origen} → {self.destino})"

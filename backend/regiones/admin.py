d7049e819163dc42eb5eecc2e397f6d2a6714fcd
sebastianulloasalas <sulloa@unsa.edu.pe>
2025-07-14 03:01:11 -0500

    Corrigiendo admin.py para que región, ruta y empresa se también estén con seguridad y solo se puedan modificar con inicio de sesion

--- backend/regiones/admin.py
+++ backend/regiones/admin.py
@@ -1,5 +1,8 @@
-from django.contrib import admin 
+from django.contrib import admin
 from .models import Region, Ruta, Empresa, Bus, Viaje
 
+admin.site.register(Region)
+admin.site.register(Ruta)
+admin.site.register(Empresa)
 admin.site.register(Bus)
 admin.site.register(Viaje)

6f15b2db3c3180a4ae29245621a184e6118f0b49
sebastianulloasalas <sulloa@unsa.edu.pe>
2025-07-15 13:54:33 -0500

    En views.py creamos dos vistas, para crear usuarios y para iniciar sesión devolviendo el token asociado

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from .serializers import RegistroSerializer
 
-# Create your views here.
+class RegistroView(APIView):
+    permission_classes = [AllowAny]  # no requiere autenticación
+
+    def post(self, request):
+        serializer = RegistroSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'username': user.username})
+        return Response(serializer.errors, status=400)
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'username': token.user.username})

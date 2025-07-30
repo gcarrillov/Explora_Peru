38b3a008ac70752d6f081b8ce7eb2623c7020a99
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-07-29 21:14:29 -0500

    feat(regiones): add new view to see random regions

--- backend/regiones/views.py
+++ backend/regiones/views.py
@@ -1,8 +1,25 @@
-from rest_framework import viewsets, status
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Region, Ruta, Empresa, Bus, Viaje
-from .serializers import RegionSerializer, RutaSerializer, EmpresaSerializer, BusSerializer, ViajeSerializer
+
+from django.http import Http404
+from django.shortcuts import redirect, reverse
+from django.views import View
+
+from urllib.parse import urlencode
+
+import random
+
+
+
+from .models import Bus, Empresa, Region, Ruta, Viaje
+from .serializers import (
+    BusSerializer,
+    EmpresaSerializer,
+    RegionSerializer,
+    RutaSerializer,
+    ViajeSerializer,
+)
 
 class RegionPorNombreAPIView(APIView):
     def get(self, request):
@@ -51,3 +68,22 @@ class BusViewSet(viewsets.ModelViewSet):
 class ViajeViewSet(viewsets.ModelViewSet):
     queryset = Viaje.objects.all()
     serializer_class = ViajeSerializer
+
+class RegionAleatoriaView(View):
+    def get(self, request):
+        # Obtener todas las regiones
+        regiones = Region.objects.all()
+        
+        if not regiones.exists():
+            raise Http404("No hay regiones disponibles")
+        
+        # Seleccionar una región aleatoria
+        region_aleatoria = random.choice(regiones)
+
+        base_url = reverse('buscar-region')
+        query_params = urlencode({'nombre': region_aleatoria.nombre})
+        redirect_url = f'{base_url}?{query_params}'
+
+        
+        # Redirigir a la vista de búsqueda con el nombre de la región
+        return redirect(redirect_url) 
